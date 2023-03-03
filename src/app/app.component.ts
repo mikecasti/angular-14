3c6f8c2e85b6d5644b0ede345b9a294e24964371
mikecasti <mikecasti@yahoo.com>
2023-03-03 17:03:13 -0400

    [20230303 task 1] examples of arrow functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test';
+
+  constructor () {
+    console.log('hi')
+  }
+
+  getName(name: string) : string {
+    return 'Hi ' + name;
+  }
+
+  // const getNameOther1 = (name:string) => { return 'Hi ' + name; }
+  // const getNameOther2 = (name:string) => 'Hi ' + name; 
+  // const getNameOther3 = () => 'Hi';
+  // const getNameOther4 = (name:string, lastName) => lastName;
+  // const getNameOther5 = num => num*2;
+  
 }
